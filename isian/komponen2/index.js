f9781d5b8278bb14fcbbf17e9b2817f9086d8afe
agusary11 <agusputu756@gmail.com>
2022-07-07 21:48:58 +0800

    UasPemogramanMobile

---
+++ isian/komponen2/index.js
@@ -0,0 +1,8 @@
+const APP_TOKEN = '6f0e98a42239f9503c64b8d0b908c950c0dcb832b6e38d7d3336a1560942d723'
+const REQUEST_HEADER = {
+    Accept: 'application/json',
+    'Content-Type' : 'application/json',
+    Authorization: `Bearer ${APP_TOKEN}`
+}
+
+export { REQUEST_HEADER }
